e10c31811e02f0587e13c6c9f074ad89275b45e3
Arashi <exterionsx@gmail.com>
2026-07-02 04:27:11 +0700

    Update

---
+++ game_data/armour_info.py
@@ -0,0 +1,19 @@
+# game_data/armour_info.py
+
+ARMOURS = {
+    "leather_armor": {
+        "def_bonus": 1,
+        "grade": "low",
+        "display_name": "Leather Armor"
+    },
+    "chainmail": {
+        "def_bonus": 2,
+        "grade": "middle",
+        "display_name": "Chainmail"
+    },
+    "plate_armor": {
+        "def_bonus": 4,
+        "grade": "high",
+        "display_name": "Plate Armor"
+    }
+}

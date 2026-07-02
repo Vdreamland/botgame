7dae1976c10a7e27897144479d25c1b905403059
Arashi <exterionsx@gmail.com>
2026-07-02 09:11:55 +0700

    Update

--- utils/ws_client.py
+++ utils/ws_client.py
@@ -1,6 +1,6 @@
-import asyncio
 import aiohttp
 import json
+import logging
 from logs.logs_network import (
     log_ws_connecting,
     log_ws_connected,
@@ -10,6 +10,8 @@ from logs.logs_network import (
     log_ws_closed
 )
 
+logger = logging.getLogger("WSClient")
+
 class ClawRoyaleWSClient:
     def __init__(self, api_key: str, api_version: str = "1.12.0"):
         self.api_key = api_key
@@ -18,16 +20,13 @@ class ClawRoyaleWSClient:
             "X-API-Key": self.api_key,
             "X-Version": self.api_version
         }
-        self.session = None
         self.ws = None
 
     async def connect(self, url: str) -> bool:
-        if self.session is None or self.session.closed:
-            self.session = aiohttp.ClientSession(headers=self.headers)
-            
+        log_ws_connecting(url)
         try:
-            log_ws_connecting(url)
-            self.ws = await self.session.ws_connect(url, heartbeat=30.0)
+            session = aiohttp.ClientSession(headers=self.headers)
+            self.ws = await session.ws_connect(url)
             log_ws_connected()
             return True
         except Exception as e:
@@ -35,37 +34,27 @@ class ClawRoyaleWSClient:
             return False
 
     async def send(self, data: dict):
-        if self.ws is not None and not self.ws.closed:
+        if self.ws:
             try:
-                await self.ws.send_json(data)
+                await self.ws.send_str(json.dumps(data))
                 log_ws_send(data)
             except Exception as e:
                 log_ws_error(str(e))
         else:
-            log_ws_error("Koneksi WebSocket belum dibuka atau terputus.")
+            log_ws_error("WebSocket connection is not open or has been disconnected.")
 
-    async def receive(self) -> dict:
-        if self.ws is not None and not self.ws.closed:
-            try:
-                msg = await self.ws.receive()
-                
-                if msg.type == aiohttp.WSMsgType.TEXT:
-                    log_ws_receive(msg.data)
-                    return json.loads(msg.data)
-                    
-                elif msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.ERROR):
-                    log_ws_closed()
-                    return None
-                    
-            except Exception as e:
-                log_ws_error(str(e))
-                return None
-        return None
+    async def receive(self):
+        if not self.ws:
+            return None
+        try:
+            msg = await self.ws.receive_str()
+            log_ws_receive(msg)
+            return json.loads(msg)
+        except Exception as e:
+            log_ws_error(str(e))
+            return None
 
     async def close(self):
-        if self.ws is not None and not self.ws.closed:
+        if self.ws:
             await self.ws.close()
             log_ws_closed()
-            
-        if self.session is not None and not self.session.closed:
-            await self.session.close()
